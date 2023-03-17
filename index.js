2d2517cce5c528b950e7e1ae96df70a8f1135d11
Injae Lee <ilee.injae@gmail.com>
2023-03-16 21:56:33 -0400

    new files

--- index.js
+++ index.js
@@ -1,49 +1,105 @@
-const modal = document.getElementById('modal')
-const modalCloseBtn = document.getElementById('modal-close-btn')
-const consentForm = document.getElementById('consent-form')
-const modalText = document.getElementById('modal-text')
-const declineBtn = document.getElementById('decline-btn')
-const modalChoiceBtns = document.getElementById('modal-choice-btns')
+import { catsData } from '/data.js'
 
-setTimeout(function(){
-    modal.style.display = 'inline'
-}, 1500)
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
-modalCloseBtn.addEventListener('click', function(){
-    modal.style.display = 'none'
-})
+emotionRadios.addEventListener('change', highlightCheckedOption)
 
-declineBtn.addEventListener('mouseenter', function(){
-    modalChoiceBtns.classList.toggle('modal-btns-reverse')
-})
+memeModalCloseBtn.addEventListener('click', closeModal)
 
-consentForm.addEventListener('submit', function(e){
-    e.preventDefault()
+getImageBtn.addEventListener('click', renderCat)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
     
-    const consentFormData = new FormData(consentForm)
-    const fullName = consentFormData.get('fullName')
-    
-    modalText.innerHTML = `
-    <div class="modal-inner-loading">
-        <img src="images/loading.svg" class="loading">
-        <p id="upload-text">Uploading your data to the dark web...</p>
-    </div>` 
-    
-    setTimeout(function(){
-        document.getElementById('upload-text').innerText = `
-        Making the sale...`
-    }, 1500)
-    
-    
-    setTimeout(function(){
-        document.getElementById('modal-inner').innerHTML = `
-        <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
-        <div class="idiot-gif">
-            <img src="images/pirate.gif">
-        </div>
-    `
-    modalCloseBtn.disabled = false
-    }, 3000)
-  
-}) 
+    if(catsArray.length === 1){
+        return catsArray[0]
+    }
+    else{
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+}
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingCatsArray 
+    }  
+}
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+function renderEmotionsRadios(cats){
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(catsData)
+
+
+
+
